1c9d377b66ada7e9d40d43f66fe8a540ac24c729
Pattanasak <Putjat145@gmail.com>
2024-04-23 14:46:43 +0700

    feat: add function for use scoll when click btn on navbar

--- src/app/componants/Navbar.tsx
+++ src/app/componants/Navbar.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from "react";
 import { IconSunMoon } from "@tabler/icons-react";
 import { useDarkModeState } from "@/store/Mode";
+import { Link } from 'react-scroll';
 
 const Navbar: React.FC = () => {
   const { isDarkMode, setIsDarkMode } = useDarkModeState();
@@ -18,11 +19,11 @@ const Navbar: React.FC = () => {
     <div className="h-[70px] flex justify-center items-center w-screen mt-8 fixed top-0 z-[1000]">
       <img src={"avatar.gif"} alt="avatar" className={`${bg_color} backdrop-blur-[10px] border-[1px] border-gray-400 shadow-xl w-[65px] h-[65px] rounded-[500px]`}/>
       <div className={`${bg_color} backdrop-blur-[10px] flex justify-between w-1/2 p-2 rounded-[100px] mx-8 border-[1px] border-gray-400 shadow-xl `} >
-        <button className={css_hover_btn()}> ABOUT ME </button>
-        <button className={css_hover_btn()}> MY PROJECT </button>
-        <button className={css_hover_btn()}> SKILLS </button>
-        {/* <button className={css_hover_btn()}> EXPERIENCE </button> */}
-        <button className={css_hover_btn()}> CONTACT </button>
+        <Link to="hello" smooth={true} duration={500} offset={-150} className={css_hover_btn()}> HELLO </Link>
+        <Link to="about" smooth={true} duration={500} offset={-150} className={css_hover_btn()}> ABOUT ME </Link>
+        <Link to="projects" smooth={true} duration={500} offset={-150} className={css_hover_btn()}> MY PROJECTS </Link>
+        <Link to="skills" smooth={true} duration={500} offset={-150} className={css_hover_btn()}> SKILLS </Link>
+        <Link to="contact" smooth={true} duration={500} offset={-150} className={css_hover_btn()}> CONTACT </Link>
       </div>
       <button
         className={`${bg_color}  backdrop-blur-[10px] border-[1px] border-gray-400 shadow-xl w-[65px] h-[65px] flex items-center justify-center rounded-[1000px]`}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,29 @@ import Exexperience from "./componants/Exexperience";
 import Projects from "./componants/Projects";
 import Contact from "./componants/Contact";
 
+import { Element } from 'react-scroll';
+
 export default function APP() {
   const { isDarkMode } = useDarkModeState();
   const bg_color = isDarkMode ? "bg-[#000000] bg-opacity-[83%] backdrop-blur-[90px]" : "bg-[#ffffff80] backdrop-blur-[200px]";
   
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between pt-[13vh] ${bg_color} `}>
-      <Home />
-      <AboutMe />
-      <Projects />
-      <Skills />
-      <Contact />
+      <Element name="hello" className="w-full flex items-center justify-center  ">
+        <Home />
+      </Element>
+      <Element name="about" className="w-full flex items-center justify-center  ">
+        <AboutMe />
+      </Element>
+      <Element name="projects" className="w-full flex items-center justify-center ">
+        <Projects />
+      </Element>
+      <Element name="skills" className="w-full flex items-center justify-center ">
+        <Skills />
+      </Element>
+      <Element name="contact" className="w-full flex items-center justify-center ">
+        <Contact />
+      </Element>
       
       {/* <Exexperience /> */}
       
