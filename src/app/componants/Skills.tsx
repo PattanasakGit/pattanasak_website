e834b2ae3a3efee47f1d50f659364a051983ff7f
Pattanasak <Putjat145@gmail.com>
2024-04-23 16:14:20 +0700

    fix: add more detail --> skills

--- src/app/componants/Skills.tsx
+++ src/app/componants/Skills.tsx
@@ -3,12 +3,28 @@ import { BGEffect, CustomText, TopicEffect } from "@/utils/ColorEffect";
 import { IconDownload } from '@tabler/icons-react';
 
 const Skills: React.FC = () => {
-    return(
-        <BGEffect className="pt-3 p-8 rounded-[50px]  w-[90%] sm:w-[85%] md:w-[90%] lg:w-[65%] xl:w-[55%] my-4">
-            <TopicEffect className="text-[35px]" > SKILLS </TopicEffect>
-            <CustomText className="ml-8 text-[20px] font-semibold my-4"> {'< Stack >'} </CustomText>
+    const mySkills = ["React", "Next.js", "Tailwind CSS", "Figma", "Express.js", "Node.js", "SQL", "MongoDB", "Docker", "JavaScript", "TypeScript", "GitHub", "GitLab", "Jira", "Scrum"];
 
-            <CustomText className="ml-8 text-[20px] font-semibold my-4"> {'< Soft >'} </CustomText>
+    // ฟังก์ชันสำหรับสุ่มสีพาสเทล
+    const getRandomPastelColor = () => {
+        const letters = 'BCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * letters.length)];
+        }
+        return color+'50';
+    };
+
+    return(
+        <BGEffect className="pt-3 p-8 rounded-[50px] w-[90%] sm:w-[85%] md:w-[90%] lg:w-[65%] xl:w-[55%] my-4">
+            <TopicEffect className="text-[35px]">SKILLS</TopicEffect>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+                {mySkills.map((skill, index) => (
+                    <div key={index} className="rounded-3xl p-4 shadow-md" style={{ backgroundColor: getRandomPastelColor() }}>
+                        <CustomText className="text-lg font-semibold text-center ">{skill}</CustomText>
+                    </div>
+                ))}
+            </div>
         </BGEffect>
     );
 };
