8779b479872b802d1b97a1a168938a9276c8492d
Pattanasak <Putjat145@gmail.com>
2024-04-28 13:03:48 +0700

    feat: add content in contact

--- src/app/componants/Contact.tsx
+++ src/app/componants/Contact.tsx
@@ -1,13 +1,48 @@
 import React from "react";
 import { BGEffect, CustomText, TopicEffect } from "@/utils/ColorEffect";
 import { IconDownload } from '@tabler/icons-react';
+import { IconMail } from '@tabler/icons-react';
+import { IconBrandLinkedin } from '@tabler/icons-react';
+import Image from 'next/image'
+
+const CustomBTN = 'w-64 h-32 p-8 bg-[#ffffff22] rounded-3xl border border-[#c6af71] flex justify-center items-center shadow-2xl hover:scale-[1.2]'
 
 const Contact: React.FC = () => {
-    return(
-        <div className="pt-3 p-8 rounded-[50px]  w-[90%] sm:w-[85%] md:w-[90%] lg:w-[65%] xl:w-[55%] my-4">
-            <TopicEffect className="text-[35px]" > CONTACT </TopicEffect>
+    const hendleClickEmail = () => {
+        window.open(`mailto:putjat145@gmail.com`);
+    }
+    const hendleClickIngedin = () => {
+        window.open("https://www.linkedin.com/in/pattanasak-atakul-353025287/", "_blank");
+    }
+  return (
+    <div className="pt-3 p-8 rounded-[50px] w-[90%] sm:w-[85%] md:w-[90%] lg:w-[65%] xl:w-[55%] my-4">
+      <TopicEffect className="text-[35px]">CONTACT</TopicEffect>
+      <div className="w-[90%] grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="flex justify-center items-center">
+          <div className="relative w-full max-w-md h-full min-h-[200px] mt-8">
+            <Image
+              src="/avatar/onefing.gif"
+              alt="avatar"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
         </div>
-    );
+        <div className="flex flex-wrap items-center  md:w-[60%] gap-4 py-[5vw]">
+          <button className={CustomBTN} onClick={hendleClickEmail}>
+            <CustomText>
+                <IconMail stroke={1.5} size={100}/>
+            </CustomText>
+          </button>
+          <button className={CustomBTN} onClick={hendleClickIngedin}>
+            <CustomText>
+                <IconBrandLinkedin stroke={1.5} size={100}/>
+            </CustomText>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
