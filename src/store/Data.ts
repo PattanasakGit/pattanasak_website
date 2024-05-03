7c4cb3f44eb42e9559b550747fcf8ab35325a47c
Pattanasak <Putjat145@gmail.com>
2024-05-03 21:19:52 +0700

    fix: change resume URL

--- src/store/Data.ts
+++ src/store/Data.ts
@@ -1,7 +1,7 @@
 export const Data = {
   links: {
     github_url: "https://github.com/PattanasakGit",
-    download_url: "https://drive.google.com/file/d/1DCYjCrmnqXuESpxHtF5EQq4LuwnlJ_Yq/view?usp=sharing",
+    download_url: "https://drive.google.com/file/d/1jPk0DgydVJAhbtjqZHldteiFGRl2NSkx/view?usp=sharing",
     mail_url: "mailto:putjat145@gmail.com",
     linkedin_url: "https://www.linkedin.com/in/pattanasak-atakul-353025287/",
   },
